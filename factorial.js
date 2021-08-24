ac54f7fc5b35b3432b56c954d258ae6631f6394a
Laura Kamphaus <lmkamphaus@gmail.com>
2021-08-24 15:12:45 -0700

    factorial recursion

--- factorial.js
+++ factorial.js
@@ -1,10 +1,13 @@
+//Write a function factorial which accepts a number and returns the factorial of that number. A factorial is the product of an integer and all the integers below it; e.g. factorial four (4!) is equal to 24, because 4 * 3 * 2 * 1 equals 24. Factorial zero is always 1.
+
 
 const factorial = (n) => {
-  if (n === 1) {
+  if (n === 1 || n === 0) {
     return 1;
   }
 
   return n * factorial(n - 1);
 }
+
 
 console.log(factorial(3)); //6
