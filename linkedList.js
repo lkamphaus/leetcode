eac74410b2fc37e2f179ad8c0114080ad27d30e2
Laura Kamphaus <lmkamphaus@gmail.com>
2021-08-30 13:29:37 -0700

    linked list push

---
+++ linkedList.js
@@ -0,0 +1,40 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+
+    return this;
+  }
+}
+
+
+let list = new LinkedList();
+list.push('HELLO');
+list.push('why');
+list.push('GOODBYE');
+list.push('why2');
+
+
+console.log(list);
