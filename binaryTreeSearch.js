36b694ec11ebecffd2d8e715421ec78dc199c212
Laura Kamphaus <lmkamphaus@gmail.com>
2021-08-31 15:42:33 -0700

    bts search

--- binaryTreeSearch.js
+++ binaryTreeSearch.js
@@ -12,18 +12,83 @@ class BST {
   }
 
   insert(val) {
-    const newNode = new Node(val);
+    let newNode = new Node(val);
 
-    if (!this.root) {
+    if (this.root === null) {
       this.root = newNode;
-    } else if (this.root.val > val) {
-      if (!this.root.left) {
-        this.root.left = val;
+      console.log(this);
+      return this;
+    }
+
+    const search = (node) => {
+      console.log('this', node);
+      if (node.val === val) {
+        return undefined;
+      }
+      if (node.val > val) {
+        if (node.left === null) {
+          node.left = newNode;
+        } else {
+          search(node.left);
+        }
+      } else if (node.val < val) {
+        if (node.right === null) {
+          node.right = newNode;
+        } else {
+          search(node.right);
+        }
       }
     }
+
+    search(this.root);
+
+    return this;
   }
 
-  search(val) {
+  find(val) {
+     let newNode = new Node(val);
 
+    if (this.root === null) {
+      this.root = newNode;
+      console.log(this);
+      return this;
+    }
+
+    const search = (node) => {
+      console.log('this', node);
+      if (node.val === val) {
+        return undefined;
+      }
+      if (node.val > val) {
+        if (node.left === null) {
+          node.left = newNode;
+        } else {
+          search(node.left);
+        }
+      } else if (node.val < val) {
+        if (node.right === null) {
+          node.right = newNode;
+        } else {
+          search(node.right);
+        }
+      }
+    }
+
+    search(this.root);
+
+    return this;
   }
+
+
 }
+
+
+let tree = new BST();
+tree.insert(10);
+tree.insert(5);
+tree.insert(2);
+tree.insert(13);
+tree.insert(11);
+tree.insert(16);
+
+console.log(tree);
