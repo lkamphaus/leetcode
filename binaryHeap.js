a7b269fa94f410b31819fae088db6a058ef8ec7c
Laura Kamphaus <lmkamphaus@gmail.com>
2021-09-01 15:05:28 -0700

    binary heap extract max

--- binaryHeap.js
+++ binaryHeap.js
@@ -32,7 +32,40 @@ class MaxBinaryHeap {
       }
     }
   }
+
+  extractMax() {
+    const max = this.values.shift();
+    const last = this.values.pop();
+
+    this.values.unshift(last);
+
+    let parentId = 0;
+
+    while(parentId < this.values.length) {
+      let childLeft = 2 * parentId + 1;
+      let childRight = 2 * parentId + 2;
+
+      const childMax = Math.max(this.values[childLeft], this.values[childRight]);
+
+      if (this.values[parentId] < childMax) {
+        let childId = this.values.indexOf(childMax);
+        this.values[childId] =  this.values[parentId];
+        this.values[parentId] = childMax;
+        parentId = childId;
+      } else {
+        break;
+      }
+    }
+
+    console.log('arr', this.values);
+    return max;
+  }
 }
 
 let MBH = new MaxBinaryHeap();
-console.log(MBH.insert(55));
+// console.log(MBH.insert(55));
+// console.log(MBH.insert(1));
+// console.log(MBH.insert(45));
+// console.log(MBH.insert(100));
+
+console.log(MBH.extractMax());
