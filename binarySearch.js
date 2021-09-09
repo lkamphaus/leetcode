ec86b7be51837a497c021deeab4b4b2e2e50fa34
Laura Kamphaus <lmkamphaus@gmail.com>
2021-09-09 10:43:44 -0700

    binary search recursive

--- binarySearch.js
+++ binarySearch.js
@@ -1,6 +1,6 @@
 //Write a function called binarySearch which accepts a sorted array and a values and returns the index at which the value exists. Otherwise, return -1
 
-const binarySearch = (arr, target) => {
+const binarySearchIterative = (arr, target) => {
   let min = 0;
   let max = arr.length - 1;
   let mid = Math.floor((max + min) / 2);
@@ -23,4 +23,21 @@ const binarySearch = (arr, target) => {
 
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5], 2)) //2
+const binarySearchRecursive = (arr, target, min = 0, max = arr.length - 1) => {
+  if (min > max) {
+    return -1;
+  }
+
+  let mid = Math.floor((max + min) / 2);
+
+  if (arr[mid] === target) {
+    return mid;
+  } if (arr[mid] < target) {
+    return binarySearchRecursive(arr, target, mid + 1, max);
+  } else {
+    return binarySearchRecursive(arr, target, min, mid - 1);
+  }
+}
+
+console.log(binarySearchIterative([1, 2, 3, 4, 5], 2)) //1
+console.log(binarySearchRecursive([1, 2, 3, 4, 5], 4)) //3
